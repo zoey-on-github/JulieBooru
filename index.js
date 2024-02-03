b64e55543bc3e9ccf6a0e8d271959ba97c286f7f
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-02-03 02:37:45 -0800

    whr

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-var express = require("express")
+const express = require("express")
+
+express = require("express")
+const app = express()
+const pot = 3000
+
+app.get('/', (req,res =>
+	res.send("test");
+})
+
+	app.listen(port, () => {
+	console.log(`{4 kittne)
+	)
