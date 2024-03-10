e162fa25fe4ea17b91ec25ee9f492aa775835e28
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2024-03-09 19:06:57 -0800

    non-working svelte front-end. woo

---
+++ frontend/vite.config.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var vite_1 = require("@sveltejs/kit/vite");
+var vite_2 = require("vite");
+exports.default = (0, vite_2.defineConfig)({
+    plugins: [(0, vite_1.sveltekit)()]
+});
--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,24 @@ const pgp = require('pg-promise')();
 const db = pgp('postgres://postgres:admin@localhost:5432/julieBooru')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.status(300).send('respond with a resource');
 });
 
 router.post('/', function(req,res,next)  {
 	//res.send("new user i guess");
         db.any('select * from users where active = $1', [true])
             .then(data => {
-                res.status(200).send('DATA:', data); // print data;
+                console.log("no error")
+                JSON.stringify(data)
+                res.status(300).send('DATA:', data); // print data;
             })
             .catch(error => {
-                res.status(200).send('ERROR:', error); // print the error;
+                console.log("test")
+                JSON.stringify(error)
+                res.status(500).send('ERROR:', error); // print the error;
             });
 })
-
+/*
 db.any('select * from users where active = $1', [true])
     .then(data => {
         console.log('DATA:', data); // print data;
@@ -25,4 +29,7 @@ db.any('select * from users where active = $1', [true])
     .catch(error => {
         console.log('ERROR:', error); // print the error;
     });
+    */
 module.exports = router;
+
+
